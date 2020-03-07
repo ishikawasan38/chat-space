9629396867da977c0f8acc740dc6c3ef36b8a90b
ishikawasan38 <61184234+ishikawasan38@users.noreply.github.com>
2020-03-07 17:20:55 +0900

    ee

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group
 
   def index
   end
@@ -32,6 +33,10 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
